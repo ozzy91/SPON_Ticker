2a5213b90ebe7601afd5e0298ded2c2d1c243133
ozzy91 <thomas.ostfeld@gmail.com>
2013-07-19 16:28:08 +0200

    The timeline scales to the available space dynamicly

--- src/com/ipol/sponticker/TickerActivity.java
+++ src/com/ipol/sponticker/TickerActivity.java
@@ -5,15 +5,15 @@ import android.os.Bundle;
 
 public class TickerActivity extends Activity {
 
+	TickerFragment fragment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ticker);
 
-		TickerFragment fragment = new TickerFragment();
+		fragment = new TickerFragment();
 		getFragmentManager().beginTransaction().add(R.id.ticker_fragment_container, fragment)
 				.commit();
 
 	}
-
 }
